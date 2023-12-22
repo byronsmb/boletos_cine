dd97757093ff7b6fb783ba01d9fc0fe7ce12f9f4
Byron Montaño <139835923+byronsmb@users.noreply.github.com>
2023-12-21 21:08:03 -0500

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
         //appBarTheme: AppBarTheme(backgroundColor: Colors.),
         useMaterial3: true,
       ),
+
       // darkTheme: ThemeData(
       //   brightness: Brightness.dark,
       //   /* dark theme settings */
